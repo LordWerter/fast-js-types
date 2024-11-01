d152952431fd59353e118dc96b6590f5033d15b6
Alexander Rudenko <learnitnow@yandex.ru>
2024-11-01 13:27:28 +0300

    feat(core): base src

--- emotion.d.ts
+++ emotion.d.ts
@@ -1,6 +1,19 @@
+import '@emotion/react'
+
 export type TSize = 'mobile' | 'laptop' | 'desktop';
 
 export type TElementProps = {
   sizeId: TSize;
   theme?: unknown;
+}
+
+declare module '@emotion/react' {
+  export interface Theme {
+    components: {
+      [key: string]: CSSRuleList;
+    }
+    containers: {
+      [key: string]: CSSRuleList;
+    }
+  }
 }
